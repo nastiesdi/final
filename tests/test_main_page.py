71d347f3e275346f5b388dc263d2957bcfad8a3b
nastiesdi <nastiesdi>
2020-01-16 16:10:47 +0300

    'add_is_element_present'

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,17 +1,23 @@
-
+from pages.main_page import MainPage
 import time
 
 
 link ='http://selenium1py.pythonanywhere.com/'
 
+def go_to_login_page(browser):
+    page = MainPage(browser, link)
 
 class TestLogin(object):
+
     def test_is_button_exist(self, browser):
-        browser.get(link)
-        # browser.find_element_by_css_selector("#login_link")
-        # browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        time.sleep(5)
-        login_link.click()
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
 
